d07d1ba657dcb78b3525dc9a4a711cc8fd898684
Chandrasekar <pchandrasekar623@gmail.com>
2021-09-16 22:08:14 +0530

    added message

--- src/main/java/TestForCICD.java
+++ src/main/java/TestForCICD.java
@@ -1,5 +1,6 @@
 public class TestForCICD {
     public static void main(String[] args){
     System.out.println("Hey Jenkins and Docker this is Devops test");
+    System.out.println("thats Cooool!");
     }
 }
