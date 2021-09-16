728f7dbc245f28de0fcde8273ce34d60edf26908
Chandrasekar <pchandrasekar623@gmail.com>
2021-09-16 22:05:38 +0530

    For test edited

--- src/main/java/TestForCICD.java
+++ src/main/java/TestForCICD.java
@@ -1,5 +1,5 @@
 public class TestForCICD {
     public static void main(String[] args){
-    System.out.println("Hey Jenkins and Docker this is jenkinspipline test");
+    System.out.println("Hey Jenkins and Docker this is Devops test");
     }
 }
